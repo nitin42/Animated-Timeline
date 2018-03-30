56406947e1ba34ee5c96d177dd3014d27e98b7e0
Nitin <tulswani19@gmail.com>
2018-03-30 19:41:54 +0530

    loop -> iterations

--- examples/Spring.js
+++ examples/Spring.js
@@ -1,82 +1,82 @@
-import React, { Component } from "react";
-
-import { boxStyles } from "./styles";
-
-const { transition } = helpers;
-
-const rebound = require("rebound");
-
-function createSpring(friction, tension) {
-  const springSystem = new rebound.SpringSystem();
-  let spring = springSystem.createSpring(friction, tension);
-
-  let id = null;
-
-  spring.init = (prevState, el, prop, options) => {
-    // el.style.transform = prop + '(' + prevState + ')'
-
-    spring.addListener({
-      onSpringUpdate: spr => {
-        var val = spr.getCurrentValue();
-        val = rebound.MathUtil.mapValueInRange(val, 1, 10, 200, 500);
-        if (prop === "rotate") val = String(val).concat("deg");
-
-        id = window.requestAnimationFrame(() => {
-          el.style.transform = prop + "(" + val + ")";
-        });
-      }
-    });
-  };
-
-  spring.stop = () => id && window.cancelAnimationFrame(id);
-
-  spring.setValue = spring.setEndValue;
-
-  spring.inMotion = spring.isAtRest;
-
-  spring.stop = () => spring.setCurrentValue(spring.getCurrentValue());
-
-  spring.restart = () => {
-    spring.setEndValue(spring.getEndValue());
-    spring.setVelocity(50);
-    // spring.setVelocity(12350)
-  };
-
-  return spring;
-}
-
-const spring = createSpring(12, 3);
-
-export class Basic extends Component {
-  state = { value: "90deg", scale: 1, motion: false };
-
-  componentDidMount() {
-    spring.init(this.state.value, this.one, "rotate");
-  }
-
-  handleUp = e => {
-    spring.setValue(5);
-    spring.setVelocity(30);
-  };
-
-  handleDown = e => {
-    spring.setValue(3);
-    spring.setVelocity(10);
-  };
-
-  render() {
-    return (
-      <div>
-        <div
-          ref={one => (this.one = one)}
-          onClick={this.handleClick}
-          onMouseUp={this.handleUp}
-          onMouseDown={this.handleDown}
-          style={{ margin: "0 auto", width: "50%", ...boxStyles }}
-        />
-        <button onClick={() => spring.stop()}>Click</button>
-        <button onClick={() => spring.restart()}>Click Again</button>
-      </div>
-    );
-  }
-}
+// import React, { Component } from "react";
+//
+// import { boxStyles } from "./styles";
+//
+// const { transition } = helpers;
+//
+// const rebound = require("rebound");
+//
+// function createSpring(friction, tension) {
+//   const springSystem = new rebound.SpringSystem();
+//   let spring = springSystem.createSpring(friction, tension);
+//
+//   let id = null;
+//
+//   spring.init = (prevState, el, prop, options) => {
+//     // el.style.transform = prop + '(' + prevState + ')'
+//
+//     spring.addListener({
+//       onSpringUpdate: spr => {
+//         var val = spr.getCurrentValue();
+//         val = rebound.MathUtil.mapValueInRange(val, 1, 10, 200, 500);
+//         if (prop === "rotate") val = String(val).concat("deg");
+//
+//         id = window.requestAnimationFrame(() => {
+//           el.style.transform = prop + "(" + val + ")";
+//         });
+//       }
+//     });
+//   };
+//
+//   spring.stop = () => id && window.cancelAnimationFrame(id);
+//
+//   spring.setValue = spring.setEndValue;
+//
+//   spring.inMotion = spring.isAtRest;
+//
+//   spring.stop = () => spring.setCurrentValue(spring.getCurrentValue());
+//
+//   spring.restart = () => {
+//     spring.setEndValue(spring.getEndValue());
+//     spring.setVelocity(50);
+//     // spring.setVelocity(12350)
+//   };
+//
+//   return spring;
+// }
+//
+// const spring = createSpring(12, 3);
+//
+// export class Basic extends Component {
+//   state = { value: "90deg", scale: 1, motion: false };
+//
+//   componentDidMount() {
+//     spring.init(this.state.value, this.one, "rotate");
+//   }
+//
+//   handleUp = e => {
+//     spring.setValue(5);
+//     spring.setVelocity(30);
+//   };
+//
+//   handleDown = e => {
+//     spring.setValue(3);
+//     spring.setVelocity(10);
+//   };
+//
+//   render() {
+//     return (
+//       <div>
+//         <div
+//           ref={one => (this.one = one)}
+//           onClick={this.handleClick}
+//           onMouseUp={this.handleUp}
+//           onMouseDown={this.handleDown}
+//           style={{ margin: "0 auto", width: "50%", ...boxStyles }}
+//         />
+//         <button onClick={() => spring.stop()}>Click</button>
+//         <button onClick={() => spring.restart()}>Click Again</button>
+//       </div>
+//     );
+//   }
+// }
--- src/spring/index.js
+++ src/spring/index.js
@@ -1,101 +1,101 @@
-const rebound = require('rebound')
-
-function createSpring(friction, tension) {
-  const springSystem = new rebound.SpringSystem()
-  let spring = springSystem.createSpring(friction, tension)
-  let id = null
-
-  spring.init = (prevState, el, prop, options) => {
-    // el.style.transform = prop + '(' + prevState + ')'
-
-    spring.addListener({
-      onSpringUpdate: spr => {
-        var val = spr.getCurrentValue()
-        // val = rebound.MathUtil.mapValueInRange(val, from[0], from[1], to[0], to[1]);
-        if (prop === 'rotate') val = String(val).concat('deg')
-
-        id = window.requestAnimationFrame(
-          () => (el.style.transform = prop + '(' + val + ')')
-        )
-      },
-    })
-  }
-
-  spring.stop = () => id && window.cancelAnimationFrame(id)
-
-  spring.setValue = spring.setEndValue
-
-  return spring
-}
-
-class App extends Component {
-  state = {
-    value: 1,
-  }
-
-  componentDidMount() {
-    spring.init(this.state.value, '#one', 'scale', {
-      mapValues: {
-        from: [],
-        to: []
-      },
-      interpolateColors: {
-        value: 1,
-        startColor: 'red',
-        endColor: 'blue',
-        from: [0, 200],
-        rgb: false,
-      }
-    })
-  }
-
-  render() {
-    return (
-      <div
-        id="one"
-        onMouseUp={() => spring.setValue(4)}
-        onMouseDown={() => spring.setValue(3)}
-      >
-        Hey, Nitin!
-      </div>
-    )
-  }
-}
-
-// Spring system for the animatione engine
-
-const spring = createSpring(friction, tension)
-
-const mapValues = {
-  from: [0, 1],
-  to: [0, 0.5]
-}
-
-const interpolateColors = {
-  value: 1,
-  startColor: 'red',
-  endColor: 'blue',
-  from: [0, 200],
-  rgb: false,
-}
-
-componentDidMount() {
-  spring.init(
-    this.state.value, // Initial value for the animation property (scale(1))
-    element, // refs or id or classname
-    properties, // 'scale'
-    { mapValues, interpolateColors }
-  )
-}
-
-handleUp() {
-  spring.setValue(5)
-}
-
-handleDown() {
-  spring.setValue(3)
-}
-
-<div id="hello-world" onMouseUp={handleUp} onMouseDown={handleDown}>
-  Hello World
-</div>
+// const rebound = require('rebound')
+//
+// function createSpring(friction, tension) {
+//   const springSystem = new rebound.SpringSystem()
+//   let spring = springSystem.createSpring(friction, tension)
+//   let id = null
+//
+//   spring.init = (prevState, el, prop, options) => {
+//     // el.style.transform = prop + '(' + prevState + ')'
+//
+//     spring.addListener({
+//       onSpringUpdate: spr => {
+//         var val = spr.getCurrentValue()
+//         // val = rebound.MathUtil.mapValueInRange(val, from[0], from[1], to[0], to[1]);
+//         if (prop === 'rotate') val = String(val).concat('deg')
+//
+//         id = window.requestAnimationFrame(
+//           () => (el.style.transform = prop + '(' + val + ')')
+//         )
+//       },
+//     })
+//   }
+//
+//   spring.stop = () => id && window.cancelAnimationFrame(id)
+//
+//   spring.setValue = spring.setEndValue
+//
+//   return spring
+// }
+//
+// class App extends Component {
+//   state = {
+//     value: 1,
+//   }
+//
+//   componentDidMount() {
+//     spring.init(this.state.value, '#one', 'scale', {
+//       mapValues: {
+//         from: [],
+//         to: []
+//       },
+//       interpolateColors: {
+//         value: 1,
+//         startColor: 'red',
+//         endColor: 'blue',
+//         from: [0, 200],
+//         rgb: false,
+//       }
+//     })
+//   }
+//
+//   render() {
+//     return (
+//       <div
+//         id="one"
+//         onMouseUp={() => spring.setValue(4)}
+//         onMouseDown={() => spring.setValue(3)}
+//       >
+//         Hey, Nitin!
+//       </div>
+//     )
+//   }
+// }
+//
+// // Spring system for the animatione engine
+//
+// const spring = createSpring(friction, tension)
+//
+// const mapValues = {
+//   from: [0, 1],
+//   to: [0, 0.5]
+// }
+//
+// const interpolateColors = {
+//   value: 1,
+//   startColor: 'red',
+//   endColor: 'blue',
+//   from: [0, 200],
+//   rgb: false,
+// }
+//
+// componentDidMount() {
+//   spring.init(
+//     this.state.value, // Initial value for the animation property (scale(1))
+//     element, // refs or id or classname
+//     properties, // 'scale'
+//     { mapValues, interpolateColors }
+//   )
+// }
+//
+// handleUp() {
+//   spring.setValue(5)
+// }
+//
+// handleDown() {
+//   spring.setValue(3)
+// }
+//
+// <div id="hello-world" onMouseUp={handleUp} onMouseDown={handleDown}>
+//   Hello World
+// </div>
