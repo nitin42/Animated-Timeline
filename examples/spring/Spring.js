0a05fd4530e71454c079488750db2befc5efeead
Nitin <tulswani19@gmail.com>
2018-04-12 16:09:36 +0530

    Init commit - Spring core

--- examples/spring/Spring.js
+++ examples/spring/Spring.js
@@ -1,41 +1,58 @@
 import React from 'react'
 
-import { createSpring } from '../../src/spring'
+import { SpringSys } from '../../src/spring'
 
 import { boxStyles } from './../styles'
 
-const spring = createSpring(4, 2)
+const spring = SpringSys(4, 2)
+
+const spring2 = SpringSys(10, 2)
 
 export class SpringSystem extends React.Component {
 	state = {
 		scale: 1,
 		value: 0,
+		rotate: '0deg',
+		backgroundColor: '#f77499',
+		translateX: 0,
 	}
 
 	componentDidMount() {
-		spring.animate('#one', this.state.scale, 'scale', {
-      mapValues: {
-        from: [0, 1],
-        to: [1, 1.5]
-      }
-    }, this.update)
-  }
-  
-  update = (style, val, { mapValues, interpolateColor }) => {
-    val = mapValues(val, 1, 1.5, 180, 360)
-    style.transform = `rotate(${val}deg)`
-    val = mapValues(val, 180, 360, 10, 20)
-    val = mapValues(val, 10, 20, 1, 3)
-    val = interpolateColor(val, '#baed9e', '#f28ec2')
-    style.backgroundColor = val    
-  }
-
-	handleChange = e => {
-		spring.seek(e.target.value)
-
-		this.setState({
-			value: e.target.value,
+		spring.animate({
+			element: this.one,
+			property: 'scale',
+			options: {
+				mapValues: {
+					from: [0, 1],
+					to: [1, 1.5],
+				},
+			},
+			interpolate: (style, val, { mapValues, interpolateColor }) => {
+				this.setState({
+					rotate: `${mapValues(val, 1, 1.5, 180, 360)}deg`,
+					translateX: mapValues(val, 1, 1.5, 15, 19),
+					backgroundColor: interpolateColor(val, '#f77499', '#85c497'),
+				})
+			},
 		})
+		
+		spring2.animate({
+			element: this.two,
+			property: 'rotate',
+			options: {
+				mapValues: {
+					from: [0, 1],
+					to: [0, 180]
+				}
+			},
+      interpolate: (style, val, { mapValues, interpolateColor }) => {
+        style.backgroundColor = interpolateColor(val, '#f77499', '#85c497', 0, 200)
+      }
+		})
+		
+		spring2.onRest = (spr) => {
+			spring2.infinite(0, 1, 1000)
+		}
 	}
 
 	render() {
@@ -43,15 +60,27 @@ export class SpringSystem extends React.Component {
 			<div style={{ margin: '0 auto', width: '50%' }}>
 				<div
 					id="one"
+					ref={one => (this.one = one)}
 					onMouseUp={() => {
 						spring.setValue(0)
-					}}
+          }}
 					onMouseDown={() => {
 						spring.setValue(1)
 					}}
+					style={{
+						...boxStyles,
+						backgroundColor: this.state.backgroundColor,
+						transform: `rotate(${this.state.rotate}) translateX(${this.state.translateX}px)`,
+					}}
+				>
+        </div>
+				<div
+					ref={two => (this.two = two)}
 					style={boxStyles}
+					onMouseUp={() => spring2.setValue(0)}
+					onMouseDown={() => spring2.setValue(1)}
 				/>
-				{/* <input type="range" min="0" max="50" value={this.state.value} onChange={this.handleChange} /> */}
+				{/* <div id='three' style={boxStyles} /> */}
 			</div>
 		)
 	}
--- src/spring/index.js
+++ src/spring/index.js
@@ -1,138 +1,239 @@
-import _R from 'rebound'
-
-import {
-  getAnimatables,
-  getCSSValue,
-  getAnimationType,
-  getOriginalTargetValue,
-  validateValue,
-} from '../core/engine'
-
 /**
 
-// Create a new spring with friction and tension
-const spring = Spring({
-  friction: 10,
-  tension: 4
-})
+TODO:
 
-// Animate the element with spring motion
-spring.animate(
-  element: '',
-  initValue: this.state.scale,
-  property: 'scale',
-  options: {
-    mapValues: {
-      from: [],
-      to: []
-    }
-  },
-  // This is called after 'setValue'. Use this callback to update styles using the newValue
-  // Use mapValues/interpolateColor helpers to convert the values and update the styles
-  onUpdate: (style, value, { mapValues, interpolateColor }) => {}
-)
+4. Refactor the listeners
 
-// Set new value and starts the motion
-spring.setValue()
+5. Flow types
 
-// Stop the motion
-spring.stop()
+6. Make the implementation concrete
 
-// Change the motion with an input value
-spring.seek()
-
-// Reset the motion
-spring.reset()
-
-// Reverse the spring motion
-spring.reverse()
-
-// Restart the spring motion
-spring.restart()
-
-// Start the motion from a value
-spring.startAt()
 */
 
-export function createSpring(friction, tension) {
-  const springSystem = new _R.SpringSystem()
-  let spring = springSystem.createSpring(friction, tension)
-  let id = null
+import _R from 'rebound'
+import invariant from 'invariant'
 
-  spring.animate = (element, state, prop, options={
-    mapValues: {
-      from: [],
-      to: []
-    }
-  }, onUpdate) => {
-    let ref = null
-    let el = null
-    let type = null
+import { getAnimatables, getCSSValue, getAnimationType, getOriginalTargetValue } from '../core/engine'
+import { noop } from '../utils/noop'
 
-    const { from, to } = options.mapValues
+const rAF = window.requestAnimationFrame
 
-    if (typeof element !== 'string') {
-      ref = element
-    } else if (typeof element === 'string') {
-      el = document.querySelector(element)
+const isColorProperty = property => property.includes('Color') || property.includes('color')
 
-      if (getAnimationType(el, prop) === 'transform') {
-        type = 'transform'
-      } else if (getAnimationType(el, prop) === 'css') {
-        type = 'css'
-      }
+const isSkewOrRotate = property => property.includes('rotate') || property.includes('skew')
 
-    } else {
-      throw new Error('Not a valid element.')
-    }
+const shouldInvokeCallback = (callback, props) => {
+	if (callback && typeof callback === 'function') callback(props)
+	else if (callback && typeof callback !== 'function')
+		console.error(`Expected callback to be a function but instead got a ${typeof callback}.`)
+}
 
-    spring.addListener({
-      onSpringUpdate: (spr) => {
-        let val = spr.getCurrentValue();
-        if (from.length !== 0 && to.length !== 0 && !prop.includes('color')) {
-          val = _R.MathUtil.mapValueInRange(val, from[0], from[1], to[0], to[1]);
-        } else if (prop.includes('color') || prop.includes('Color')) {
-          val = _R.util.interpolateColor(val, '#f2a4eb', '#4c364a')
-        }
+const validateValue = (property, value) => {
+	// Color properties are validated and converted to RGB by `interpolateColor` option
+	if (isColorProperty(property)) return value
+	else if (isSkewOrRotate(property))
+		// TODO: Add conversion for degrees to radians and vice-versa
+		return String(value).concat('deg')
+	else return String(value).concat('px')
+}
 
-        if (typeof prop !== 'function') {
-          if (prop.includes('rotate') || prop.includes('skew')) val = String(val).concat('deg')
+const applyInitialProps = (el, { property, value, type }) => {
+	const newValue = validateValue(property, value)
 
-          if (prop.includes('translate')) val = String(val).concat('px')
+	if (type === 'transform') {
+		el.style.transform = `${property}(${newValue})`
+	} else if (type === 'css') {
+		el.style[property] = newValue
+	}
+}
 
-        }
+export function SpringSys(friction = 10, tension = 5) {
+	invariant(
+		typeof friction === 'number',
+		`Expected 'friction' value to be a number but instead got a ${typeof friction}`
+	)
 
-        id = window.requestAnimationFrame(() => {
-          if (type === 'transform') {
-            onUpdate(el.style, val, { mapValues: _R.MathUtil.mapValueInRange, interpolateColor: _R.util.interpolateColor})
-            if (!el.style['transform'].includes(prop)) {
-              el.style['transform'] = el.style['transform'].concat(`${prop}(${val})`)
-            } else {
-              el.style['transform'] = `${prop}(${val})`
-            }
-          } else if (type === 'css') {
-            el.style[prop] = `${val}`
-          }
-        })
-      }
-    })
-  }
+	invariant(typeof tension === 'number', `Expected 'tension' value to be a number but instead got a ${typeof tension}`)
 
-  spring.stop = () => spring.setCurrentValue(spring.getCurrentValue())
+	// Main spring system instance
+	const spring = new _R.SpringSystem().createSpring(friction, tension)
 
-  spring.reset = () => {
-    spring.setCurrentValue(-1)
-  }
+	const mapValues = _R.MathUtil.mapValueInRange
 
-  spring.reverse = () => {
-    spring.setEndValue(-(spring.getCurrentValue()))
-  }
+	const interpolateColor = _R.util.interpolateColor
 
-  spring.seek = (val) => {
-    spring.setEndValue(val)
-  }
+	let id = null
 
-  spring.setValue = spring.setEndValue
+	spring.animate = ({
+		element = '',
+		property,
+		options = {
+			mapValues: { from: [0, 1], to: [1, 1.5] },
+			interpolateColor: { colors: ['#183a72', '#85c497'], range: [] },
+		},
+		interpolate = noop,
+	}) => {
+		invariant(
+			!Array.isArray(element) || typeof element === 'string' || typeof element === 'object',
+			'Can only pass a selector (id or class) or a reference to the element'
+		)
 
-  return spring
+		invariant(typeof property === 'string', `Expected property to be a string but instead got a ${typeof property}.`)
+
+		invariant(
+			typeof interpolate === 'function',
+			`Expected onUpdate to be a function but instead got a ${typeof interpolate}.`
+		)
+
+		// Reference to the element which will be animated
+		let el = null
+
+		// Property type (css or transform)
+		let type = null
+
+		if (typeof element === 'object') {
+			// must be a 'ref'
+			el = element
+		} else if (typeof element === 'string') {
+			// id or classname
+			el = document.querySelector(element)
+		}
+
+		if (getAnimationType(el, property) === 'transform') {
+			type = 'transform'
+		} else if (getAnimationType(el, property) === 'css') {
+			type = 'css'
+		}
+
+		// Set the initial state of the animation property of the element we want to animate
+		applyInitialProps(el, {
+			property,
+			value: isColorProperty(property) ? options.interpolateColor.colors[0] : options.mapValues.to[0],
+			type,
+		})
+
+		spring.addListener({
+			onSpringActivate: spr => shouldInvokeCallback(spring.onStart, spr),
+			onSpringAtRest: spr => shouldInvokeCallback(spring.onRest, spr),
+			onSpringUpdate: spr => {
+				shouldInvokeCallback(spring.onUpdate, spr)
+
+				let val = spr.getCurrentValue()
+
+				if (!isColorProperty(property)) {
+					const { from, to } = options.mapValues
+
+					val = mapValues(val, from[0], from[1], to[0], to[1])
+				} else if (isColorProperty(property)) {
+					const { colors, range } = options.interpolateColor
+
+					if (range && (Array.isArray(range) && range.length === 2)) {
+						val = interpolateColor(val, colors[0], colors[1], range[0], range[1])
+					} else {
+						// Ignore the input range
+						val = interpolateColor(val, colors[0], colors[1])
+					}
+				}
+
+				if (isSkewOrRotate(property)) {
+					val = String(val).concat('deg')
+				} else if (property.includes('translate')) val = String(val).concat('px')
+
+				id = rAF(() => {
+					interpolate(
+						// Pass the style object
+						el.style,
+						// Values should be unitless
+						String(val).replace('deg', '') || String(val).replace('px', ''),
+						// Options to map values from one range to another (for numbers and hex codes)
+						{
+							mapValues: _R.MathUtil.mapValueInRange,
+							interpolateColor: _R.util.interpolateColor,
+						}
+					)
+
+					if (type === 'transform') {
+						if (!el.style['transform'].includes(property)) {
+							el.style['transform'] = el.style['transform'].concat(`${property}(${val})`)
+						} else {
+							el.style['transform'] = `${property}(${val})`
+						}
+					} else if (type === 'css') {
+						el.style[property] = `${val}`
+					}
+				})
+			},
+		})
+
+		// Support chaining
+		return spring
+	}
+
+	// Set a new value
+	spring.setValue = spring.setEndValue
+
+	// Set a new value and velocity
+	spring.startWithVelocity = ({ value, velocity }) => {
+		spring.setValue(value)
+		spring.setVelocity(velocity)
+	}
+
+	// Start the motion with a value
+	spring.startAt = val => spring.setValue(val)
+
+	// Stop the motion
+	spring.stop = () => spring.setCurrentValue(spring.getCurrentValue())
+
+	// Reset the motion
+	spring.reset = () => {
+		spring.setCurrentValue(-1)
+	}
+
+	// Reverse the motion
+	spring.reverse = () => {
+		spring.setValue(-spring.getCurrentValue())
+	}
+
+	// Change the position of an element along with its motion
+	spring.seek = val => {
+		spring.setValue(val)
+	}
+
+	// Start the motion
+	spring.start = () => {
+		const value = spring.getEndValue() - spring.getCurrentValue()
+		
+		spring.setValue(value)
+	}
+
+	spring.infinite = (startValue, endValue, duration) => {
+		let id = null
+
+		spring.setValue(startValue)
+
+		id = setTimeout(() => {
+			spring.setValue(endValue)
+		}, duration || 1200)
+
+		return id
+	}
+
+	spring.oncancel = () => {
+		let res = null
+
+		function createPromise() {
+			return window.Promise && new Promise(resolve => (res = resolve))
+		}
+
+		const promise = createPromise()
+
+		spring.removeAllListeners()
+		window.cancelAnimationFrame(id)
+
+		res({ msg: 'Animation cancelled.' })
+
+		return promise
+	}
+
+	return spring
 }
