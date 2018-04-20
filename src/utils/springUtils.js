15415f27471a79c322b0a99689b4a2c43b26c1fa
Nitin <tulswani19@gmail.com>
2018-04-20 23:01:57 +0530

    prettier 🎉

--- src/utils/springUtils.js
+++ src/utils/springUtils.js
@@ -1,7 +1,7 @@
 const withUnit = (value, unit) => {
-	value = String(value)
-	if (value.includes(unit)) return value
-	return value.concat(unit)
+  value = String(value)
+  if (value.includes(unit)) return value
+  return value.concat(unit)
 }
 
 // Helpers for assigning units
@@ -17,7 +17,7 @@ const UNITS = /([\+\-]?[0-9#\.]+)(%|px|em|rem|in|cm|mm|vw|vh|vmin|vmax|deg|rad|t
 
 // Get the unit from value
 export const parseValue = value => {
-	value = String(value)
-	const split = UNITS.exec(value.replace(/\s/g, ''))
-	if (split) return split
+  value = String(value)
+  const split = UNITS.exec(value.replace(/\s/g, ''))
+  if (split) return split
 }
